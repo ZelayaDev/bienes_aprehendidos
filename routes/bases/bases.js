2c10b79cf3d1591bb545f8d6ff1fed0705310719
rigoberto.zelaya <rigoberto.zelaya@TEL-004478>
2021-03-10 13:51:45 -0500

    Validacion de Nombre de base y Orden

--- routes/bases/bases.js
+++ routes/bases/bases.js
@@ -1,41 +1,84 @@
 const router = require("express").Router();
-const {getAllBases, getBasesbyIdRegion,  insertBases, updateBases, validarRegion} = require('./model');
+const {
+  getAllBases,
+  getBasesbyIdRegion,
+  insertBases,
+  updateBases,
+  validarBases,
+} = require("./model");
 
-router.get("/", async (req, res)  => {
-   console.log(req.query)
-   if(req.query === "region") {
-       const bases = await getBasesbyIdRegion(req.query.region)
-       res.status(200).json(bases)
-   } else {
-    const bases = await getAllBases()
-    res.status(200).json(bases)
-   }
-  
-}).post("/", async (req, res) => {
-    console.log(req.body)
+router
+  .get("/", async (req, res) => {
+    console.log(req.query);
+    if (req.query === "region") {
+      const bases = await getBasesbyIdRegion(req.query.region);
+      res.status(200).json(bases);
+    } else {
+      const bases = await getAllBases();
+      res.status(200).json(bases);
+    }
+  })
+  .post("/", async (req, res) => {
+    console.log(req.body);
 
-    const {id_region, id_provincia, id_tipologia, id_zona, nombre_Base, orden_base} = req.body; 
+    const {
+      id_region,
+      id_provincia,
+      id_tipologia,
+      id_zona,
+      nombre_Base,
+    } = req.body;
     const status = 1;
-    const verificaRegion = await validarRegion(id_region);
-    if (verificaRegion) return res.status(400).json("Verifique Region")
 
     try {
-        await insertBases(id_region, id_provincia, id_tipologia, id_zona, nombre_Base, orden_base, status)
-        res.status(200).json("Registro Creado")
+      const busquedaBase = await validarBases(nombre_Base);
+      if (busquedaBase.length) return res.status(406).json("Base existe");
+
+      await insertBases(
+        id_region,
+        id_provincia,
+        id_tipologia,
+        id_zona,
+        nombre_Base,
+        status
+      );
+      res.status(200).json("Registro Creado");
     } catch (error) {
-        res.status(500).json("Error")
+      res.status(500).json("Error");
     }
-}).put("/", async (req, res) => {
-    console.log(req.body)
-    const {id_region, id_provincia, id_tipologia, id_zona, nombre_Base, orden_base, id_base} = req.body; 
+  })
+  .put("/", async (req, res) => {
+    console.log("body: ", req.body);
+    const {
+      id_region,
+      id_provincia,
+      id_tipologia,
+      id_zona,
+      nombre_Base,
+      orden_base,
+      id_base,
+    } = req.body;
 
     try {
-        await updateBases(id_region, id_provincia, id_tipologia, id_zona, nombre_Base, orden_base, id_base)
-        res.status(200).json("Registro actualizado")
+      const update = await updateBases(
+        id_region,
+        id_provincia,
+        id_tipologia,
+        id_zona,
+        nombre_Base,
+        orden_base,
+        id_base
+      );
+      if (
+        update === "base ya existe" ||
+        update === "orden ya existe" ||
+        update === 0
+      )
+        return res.status(400).json(update === 0 ? "error en update" : update);
+      res.status(200).json("Registro actualizado");
     } catch (error) {
-        res.status(500).json("Error")
+      res.status(500).json("Error");
     }
-})
-
+  });
 
 module.exports = router;
--- routes/bases/model.js
+++ routes/bases/model.js
@@ -1,89 +1,167 @@
-const {database} = require("../../database/database");
+const { database } = require("../../database/database");
 
 const getAllBases = async () => {
-    return database
-    .select("a.*","b.Nombre_Region","c.nombre_zona","d.Nombre_Provincia","e.Descripcion_Tipologia","e.Abreviatura_Tipologia") 
+  return database
+    .select(
+      "a.*",
+      "b.Nombre_Region",
+      "c.nombre_zona",
+      "d.Nombre_Provincia",
+      "e.Descripcion_Tipologia",
+      "e.Abreviatura_Tipologia"
+    )
     .from("bases_aeronaval as a")
-    .innerJoin("regiones_aeronaval as b","b.Id_Region_Aeronaval","a.Id_Region" )
-    .innerJoin("zonas_aeronaval as c", "a.Id_Zona","c.Id_Zona_Aeronaval")
-    .innerJoin("provincias_aeronaval as d","a.Id_Provincia","d.Id_Provincia_Aeronaval")
-    .innerJoin("tipologia_aeronaval as e","a.Id_Tipologia","e.Id_Tipologia_Aeronaval")
-    .orderBy("a.Id_Region","a.Orden_Base")
-    .then((bases) =>{
-        return bases
+    .innerJoin(
+      "regiones_aeronaval as b",
+      "b.Id_Region_Aeronaval",
+      "a.Id_Region"
+    )
+    .innerJoin("zonas_aeronaval as c", "a.Id_Zona", "c.Id_Zona_Aeronaval")
+    .innerJoin(
+      "provincias_aeronaval as d",
+      "a.Id_Provincia",
+      "d.Id_Provincia_Aeronaval"
+    )
+    .innerJoin(
+      "tipologia_aeronaval as e",
+      "a.Id_Tipologia",
+      "e.Id_Tipologia_Aeronaval"
+    )
+    .orderBy("a.Id_Region", "a.Orden_Base")
+    .then((bases) => {
+      return bases;
     })
     .catch((error) => {
-        return error;
-    })
-}
+      return error;
+    });
+};
 
 const getBasesbyIdRegion = async (id_region) => {
-    return database
-    .select("a.*","b.Nombre_Region","c.nombre_zona","d.Nombre_Provincia","e.Descripcion_Tipologia","e.Abreviatura_Tipologia") 
+  return database
+    .select(
+      "a.*",
+      "b.Nombre_Region",
+      "c.nombre_zona",
+      "d.Nombre_Provincia",
+      "e.Descripcion_Tipologia",
+      "e.Abreviatura_Tipologia"
+    )
     .from("bases_aeronaval as a")
-    .innerJoin("regiones_aeronaval as b","b.Id_Region_Aeronaval","a.Id_Region" )
-    .innerJoin("zonas_aeronaval as c", "a.Id_Zona","c.Id_Zona_Aeronaval")
-    .innerJoin("provincias_aeronaval as d","a.Id_Provincia","d.Id_Provincia_Aeronaval")
-    .innerJoin("tipologia_aeronaval as e","a.Id_Tipologia","e.Id_Tipologia_Aeronaval")
-    .where("b.Id_Region_Aeronaval","=",id_region)
-    .orderBy("a.Id_Region","a.Orden_Base")
-    .then((bases) =>{
-        return bases
+    .innerJoin(
+      "regiones_aeronaval as b",
+      "b.Id_Region_Aeronaval",
+      "a.Id_Region"
+    )
+    .innerJoin("zonas_aeronaval as c", "a.Id_Zona", "c.Id_Zona_Aeronaval")
+    .innerJoin(
+      "provincias_aeronaval as d",
+      "a.Id_Provincia",
+      "d.Id_Provincia_Aeronaval"
+    )
+    .innerJoin(
+      "tipologia_aeronaval as e",
+      "a.Id_Tipologia",
+      "e.Id_Tipologia_Aeronaval"
+    )
+    .where("b.Id_Region_Aeronaval", "=", id_region)
+    .orderBy("a.Id_Region", "a.Orden_Base")
+    .then((bases) => {
+      return bases;
     })
     .catch((error) => {
-        return error;
-    })
-}
+      return error;
+    });
+};
 
-const insertBases = async (id_region, id_provincia, id_tipologia, id_zona, nombre_Base, orden_base, status) =>{
-    return database("bases_aeronaval").insert({
-        Id_Region:id_region,
-        Id_Provincia:id_provincia,
-        Id_Tipologia: id_tipologia,
-        Id_Zona: id_zona,
-        Nombre_Base: nombre_Base,
-        Orden_Base: orden_base,
-        Estatus_Base:status
-    }).then(response => {
-        return response
-    }).catch(error => {
-        console.log(error)
-        return error
-    })
-}
-
-
-const updateBases = (id_region, id_provincia, id_tipologia, id_zona, nombre_Base, orden_base, id_base) => {
-    return database("bases_aeronaval").where("Id_Bases_Aeronaval","=",id_base).update({
-        Id_Region:id_region,
-        Id_Provincia:id_provincia,
-        Id_Tipologia: id_tipologia,
-        Id_Zona: id_zona,
-        Nombre_Base: nombre_Base,
-        Orden_Base: orden_base
-    }).then(response => {
-        return response
-    }).catch(error => {
-        console.log(error)
-        return error
-    })
-}
-
-const validarRegion = (id_region) => {
-    if (id_region) {
-        const region_id = parseInt(id_region)
-        return database.select("*").from("regiones_aeronaval").where("Id_Region_Aeronaval", "=", region_id)
-        .then(region => {
-            if (region.length){
-                if (region[0].Estatus_Region === 0) return "Region esta desactivada";
-                return "Region Existe";
-            } else return "Region no existe";
+const insertBases = async (
+  id_region,
+  id_provincia,
+  id_tipologia,
+  id_zona,
+  nombre_Base,
+  status
+) => {
+  return database
+    .max("Orden_Base as orden")
+    .from("bases_aeronaval")
+    .then((response) => {
+      return database("bases_aeronaval")
+        .insert({
+          Id_Region: id_region,
+          Id_Provincia: id_provincia,
+          Id_Tipologia: id_tipologia,
+          Id_Zona: id_zona,
+          Nombre_Base: nombre_Base,
+          Orden_Base: response.length === 0 ? 1 : response[0].orden + 0.5,
+          Estatus_Base: status,
         })
-    }else return "Valor de Region incorrecto";
-}   
+        .then((response) => {
+          return response;
+        })
+        .catch((error) => {
+          console.log(error);
+          return error;
+        });
+    })
+    .catch((err) => err);
+};
+
+const updateBases = (
+  id_region,
+  id_provincia,
+  id_tipologia,
+  id_zona,
+  nombre_Base,
+  orden_base,
+  id_base
+) => {
+  return database
+    .select("*")
+    .from("bases_aeronaval")
+    .where("Nombre_Base", "=", nombre_Base)
+    .then((respuesta) => {
+      if (respuesta.length > 0) return "base ya existe";
+      return database
+        .select("*")
+        .from("bases_aeronaval")
+        .where("Orden_Base", "=", orden_base)
+        .then((response) => {
+          if (response.length > 0) return "orden ya existe";
+          return database("bases_aeronaval")
+            .where("Id_Bases_Aeronaval", "=", id_base)
+            .update({
+              Id_Region: id_region,
+              Id_Provincia: id_provincia,
+              Id_Tipologia: id_tipologia,
+              Id_Zona: id_zona,
+              Nombre_Base: nombre_Base,
+              Orden_Base: orden_base,
+            })
+            .then((response) => {
+              console.log("response", response);
+              return response;
+            })
+            .catch((error) => {
+              console.log(error);
+              return error;
+            });
+        })
+        .catch((error) => error);
+    })
+    .catch((error) => error);
+};
+
+const validarBases = (texto) => {
+  return database
+    .select("*")
+    .from("bases_aeronaval")
+    .where("Nombre_Base", "=", texto)
+    .then((respuesta) => respuesta)
+    .catch((err) => err);
+};
 
 module.exports.getAllBases = getAllBases;
 module.exports.getBasesbyIdRegion = getBasesbyIdRegion;
 module.exports.insertBases = insertBases;
 module.exports.updateBases = updateBases;
-module.exports.validarRegion = validarRegion;
+module.exports.validarBases = validarBases;
