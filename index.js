d9cd57c1f37f0a697597108f825b86d49a54bbae
Anthony Cardoze <anthonycardoze@gmail.com>
2021-04-15 12:00:38 -0500

    agregando cambios de variable de entorno y depurar dependencias

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
-//const dotenv = require("dotenv");
+const compression = require("compression");
+const helmet = require("helmet");
+require("dotenv").config();
 
 //Middlewares
 
-//dotenv.config();
 const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "5mb" }));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 app.use(cors());
+app.use(helmet());
+app.use(compression());
+
 const port = process.env.PORT || 5000;
 app.listen(port, (error) => {
   if (error) throw error;
-  console.log(`Servidor corriendo : ${port}`);
+  console.log(`Servidor corriendo : localhost:${port}`);
 });
 
 //Rutas
